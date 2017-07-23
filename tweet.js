03939f7dd809d6e04aaa8023931a98954663acd8
KASANAMI Masaaki <saw.toothed.coastline.12@gmail.com>
2017-07-23 17:14:01 +0900

    tweet.jsを外部から呼び出せるように変更

--- tweet.js
+++ tweet.js
@@ -1,15 +1,20 @@
-var twit = require('twit');
-var fs = require('fs');
+var tweet = function(tweetStr){
+    var twit = require('twit');
+    var fs = require('fs');
 
-var SECRET = JSON.parse(fs.readFileSync('./SECRET.json', 'utf8'));
+    var SECRET = JSON.parse(fs.readFileSync('./SECRET.json', 'utf8'));
 
-var T = new twit({
-    consumer_key:           SECRET.consumer_key,
-    consumer_secret:        SECRET.consumer_secret,
-    access_token:           SECRET.access_token,
-    access_token_secret:    SECRET.access_token_secret
-})
+    var T = new twit({
+        consumer_key:           SECRET.consumer_key,
+        consumer_secret:        SECRET.consumer_secret,
+        access_token:           SECRET.access_token,
+        access_token_secret:    SECRET.access_token_secret
+    })
 
-T.post('statuses/update', { status: Date.now() }, function(err, data, response){
-    console.log(data);
-});
+    T.post('statuses/update', { status: tweetStr }, function(err, data, response){
+        console.log(data);
+    });
+}
+
+module.exports = tweet;
+
