1abdbafefaf2edd8f2c6398e945795c36f26b41a
KASANAMI Masaaki <saw.toothed.coastline.12@gmail.com>
2017-07-23 16:56:37 +0900

    dashButtonのmacアドレス変更

--- dashButton.js
+++ dashButton.js
@@ -1,6 +1,6 @@
 var dash_button = require('node-dash-button');
 
-var dash = dash_button('fc:4a:e9:de:ad:05', null, null, 'all');
+var dash = dash_button('34:d2:70:02:c8:2b', null, null, 'all');
 dash.on('detected', function(){
     console.log("pushed!!");
 });
