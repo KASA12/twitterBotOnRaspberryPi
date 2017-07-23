f74dce1751abadc7c884ff5bfd7fcdd077f2bfc8
KASANAMI Masaaki <saw.toothed.coastline.12@gmail.com>
2017-07-23 18:16:55 +0900

    日時、回数が更新されないバグを修正

--- main.js
+++ main.js
@@ -1,20 +1,23 @@
 var tweet = require('./tweet.js');
 var dashButton = require('./dashButton.js');
 
-var date = new Date();
-
 var kaeritamiCount = 1;
-var kaeritamistr = 
+const kaeritamistr = 
 "＿人人人人人人人人人人＿\n\
 ＞　おうちかえりたい　＜\n\
 ￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣\n";
-var timeStr =
-    date.getFullYear() + "/" + zeroPadding(date.getMonth()+1, 2) + "/" + zeroPadding(date.getDate(), 2) + " " +
-    zeroPadding(date.getHours(), 2) + ":" + zeroPadding(date.getMinutes(), 2);
-var postStr =
-"【" + kaeritamiCount + "回目】" + timeStr + "\n\n" + kaeritamistr + "\n";
 
 dashButton(function(){
+    var date = new Date();
+    var timeStr =
+        date.getFullYear() + "/" + 
+        zeroPadding(date.getMonth()+1, 2) + "/" + 
+        zeroPadding(date.getDate(), 2) + " " +
+        zeroPadding(date.getHours(), 2) + ":" + 
+        zeroPadding(date.getMinutes(), 2) + "." + 
+        zeroPadding(date.getSeconds(), 2);
+    var postStr =
+        "【" + kaeritamiCount + "回目】" + timeStr + "\n\n" + kaeritamistr + "\n\n";
     tweet(postStr);
     kaeritamiCount++;
 });
