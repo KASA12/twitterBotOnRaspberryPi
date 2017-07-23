9ff868c9b61fda93d966989f1e9e5de8b0e9bf7b
KASANAMI Masaaki <saw.toothed.coastline.12@gmail.com>
2017-07-23 17:26:35 +0900

    dashButton.jsを外部から呼び出せるように変更

--- dashButton.js
+++ dashButton.js
@@ -1,6 +1,9 @@
-var dash_button = require('node-dash-button');
+var dashButton = function(callback){
+    var dash_button = require('node-dash-button');
+    var dash = dash_button('34:d2:70:02:c8:2b', null, null, 'all');
+    dash.on('detected', function(){
+        callback && callback();
+    });
+}
 
-var dash = dash_button('34:d2:70:02:c8:2b', null, null, 'all');
-dash.on('detected', function(){
-    console.log("pushed!!");
-});
+module.exports = dashButton;
